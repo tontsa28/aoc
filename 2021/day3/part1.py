965732aefd094010f505774e27d5b3ee34ee5584
tontsa28 <xdman7917@gmail.com>
2021-12-05 18:08:43 +0200

    Day 3 part 1 solution

--- 2021/day3/part1.py
+++ 2021/day3/part1.py
@@ -1,35 +1,15 @@
+# Import Counter
+from collections import Counter
+
+# Open input file
 with open("input.txt") as f:
-    lines = f.readlines()
+    lines = list(map(str.strip, f.readlines()))
 
-    nth = 0
-    gamma = ""
-    epsilon = ""
-    length = len(lines)
-    
-    for i in range(length):
-        nth_line = lines[nth]
-        bit = 0
-        char = ""
-        nthbit = nth_line[bit]
-        gamma_count = 0
-        epsilon_count = 0
-        if nthbit == "1":
-            gamma_count = gamma_count + 1
-            char += "1"
-        elif nthbit == "0":
-            epsilon_count = epsilon_count + 1
-            char += "0"
-        if gamma_count > epsilon_count:
-            gamma += char
-        else:
-            epsilon += char
-        char = ""
-        nth = nth + 1
-        bit = bit + 1
+    # Calculate gamma and epsilon
+    gamma = int("".join([Counter(x).most_common(1)[0][0] for x in list(zip(*lines))]), 2)
+    epsilon = int(len(lines[0])*"1", 2) - gamma
 
-    print(gamma)
-    print(epsilon)
-    print(gamma_count)
-    print(epsilon_count)
-    print(bit)
-    print(nth)
+    # Print results
+    print("Gamma: " + str(gamma))
+    print("Epsilon: " + str(epsilon))
+    print("Submarine power consumption: " + str(gamma * epsilon))
