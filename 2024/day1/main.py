6115aab250c18fc9e9d385ffbb1732c7776b8d9d
tontsa28 <miika@tontsa.fi>
2024-12-02 18:52:58 +0200

    feat: 2024 day 1 solution

---
+++ 2024/day1/main.py
@@ -0,0 +1,23 @@
+def solve_part1(left: list[int], right: list[int]) -> int:
+    left.sort()
+    right.sort()
+    return sum([abs(l - r) for l, r in zip(left, right)])
+
+def solve_part2(left: list[int], right: list[int]) -> int:
+    return sum([l * right.count(l) for l in left])
+
+def main():
+    left: list[int] = []
+    right: list[int] = []
+
+    with open('input.txt') as file:
+        for line in file:
+            splitted = line.split('   ')
+            left.append(int(splitted[0]))
+            right.append(int(splitted[1].removesuffix('\n')))
+
+    print(solve_part1(left, right))
+    print(solve_part2(left, right))
+
+if __name__ == '__main__':
+    main()
